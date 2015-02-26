7ede95cc35f232b6c740bf9a5e2282c1dcd4b372
Curtis Belmonte <curtislb@princeton.edu>
2015-02-25 23:10:51 -0500

    Adds preliminary ROC curves

--- classification.py
+++ classification.py
@@ -1,4 +1,7 @@
 from email_process import read_bagofwords_dat, read_classes_txt
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt
+import numpy
 
 def train_classifier(classifier):
     filename = 'trec07p_data/Train/train_emails_bag_of_words_200.dat'
@@ -13,8 +16,14 @@ def test_classifier(classifier):
     
     classes = read_classes_txt('trec07p_data/Test/test_emails_classes_0.txt')
     num_correct = 0
-    for i in range(len(predictions)):
+    for i in xrange(len(predictions)):
         if predictions[i] == classes[i]:
             num_correct += 1
     
     print 'Accuracy:', (float(num_correct) / float(len(predictions)))
+    
+    fpr, tpr, _ = roc_curve(classes, predictions)
+    plt.plot(fpr, tpr, '.-')
+    x = [0.01 * n for n in xrange(101)]
+    plt.plot(x, x, '--')
+    plt.savefig('roc_curve.png')
